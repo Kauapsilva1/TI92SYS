e19726bc1ae0678102744aa02772cf89006adfaf
kaua.psilva1 <kaua.psilva1@ITQ626042W10-1>
2023-01-31 11:48:50 -0300

    Adicionar arquivos de projeto.

---
+++ TI92APP/FrmPrincipal.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace TI92APP
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.txtSelecao = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnSair.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSair.Location = new System.Drawing.Point(191, 334);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(101, 30);
+            this.btnSair.TabIndex = 40;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(73, 87);
+            this.txtNome.MaxLength = 45;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(187, 20);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(73, 130);
+            this.txtCpf.MaxLength = 11;
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(100, 20);
+            this.txtCpf.TabIndex = 3;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(70, 71);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 4;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(73, 114);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(27, 13);
+            this.lblCpf.TabIndex = 1;
+            this.lblCpf.Text = "CPF";
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.Location = new System.Drawing.Point(73, 199);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(219, 95);
+            this.lstResultado.TabIndex = 6;
+            this.lstResultado.SelectedIndexChanged += new System.EventHandler(this.lstResultado_SelectedIndexChanged);
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(191, 130);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(101, 29);
+            this.btnGravar.TabIndex = 2;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // txtSelecao
+            // 
+            this.txtSelecao.FormattingEnabled = true;
+            this.txtSelecao.Location = new System.Drawing.Point(73, 301);
+            this.txtSelecao.Name = "txtSelecao";
+            this.txtSelecao.Size = new System.Drawing.Size(219, 17);
+            this.txtSelecao.TabIndex = 41;
+            // 
+            // FrmPrincipal
+            // 
+            this.AcceptButton = this.btnGravar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnSair;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSelecao);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.lstResultado);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnSair);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.ListBox txtSelecao;
+    }
+}
+
---
+++ TI92APP/FrmPrincipal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TI92APP
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            btnGravar.Enabled = false;
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNome.Text!= string.Empty)
+            { 
+                btnGravar.Enabled = true;
+            }
+            else
+            { 
+                btnGravar.Enabled = false; 
+            }
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text == string.Empty)
+            {
+                string nome = txtNome.Text;
+                string cpf = txtCpf.Text;
+                int num = lstResultado.Items.Add(nome + " - "+ cpf);
+              //  MessageBox.Show(num.ToString());
+                lstResultado.Items.Add(nome + " - " + cpf);
+                txtCpf.Clear();
+                txtNome.Clear();
+            }
+            // Regra de negócio
+            // 1 - apenas um cliente por CPF na lista
+            // 2 - minimizar clicks
+            // 3 - todas as letras do nome em maiúsculas
+        }
+
+        private void lstResultado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtSelecao.Text = lstResultado.SelectedItem.ToString();
+        }
+    }
+}
